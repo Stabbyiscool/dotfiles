b6137cc43cc83960c13bda3e34cc23b1d176a801
Stabbyiscool <elocreatorelo@gmail.com>
2025-02-03 06:57:46 -0600

    wow 4th dot files

---
+++ .config/sway/sway_status.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+while true; do
+    VOL=$(pamixer --get-volume-human)
+    RAM=$(free -h | awk '/^Mem:/ {print $3 "/" $2}')
+    DATE=$(date +"%Y-%m-%d %H:%M:%S")
+    CPU=$(top -bn1 | grep "Cpu(s)" | awk '{printf "%.1f%%", $2 + $4}')
+    REC=$(pgrep -x "wf-recorder" > /dev/null && echo "RECORDING!")
+
+    RESPONSE=$(curl -s -m 5 -w "%{http_code}" -o /dev/null https://stabosa.fun)
+    if [ "$RESPONSE" != "200" ]; then
+        ERROR_MSG=$(curl -s https://stabosa.fun | grep -oP '(?<=<title>).*?(?=</title>)')
+        echo "MAKAN SERVERS ARE DOWN! ${ERROR_MSG:-No response or timeout.}"
+        sleep 5
+        continue
+    fi
+
+    USRTXT=$(curl -s http://192.168.1.44:8252/ | jq -r .latest_text)
+    [ -z "$USRTXT" ] && USRTXT="NAN"
+
+    echo "$USRTXT${REC:+ | $REC} | $DATE | CPU: $CPU | RAM: $RAM | VOL: $VOL"
+
+    sleep 1
+done
--- readme.md
+++
@@ -1,3 +0,0 @@
-## Dotfiles
-i reinstall arch ALOT and i hate having to archive these onto a separate drive so i just made a repo
-![thumb up](https://media1.tenor.com/m/DtD4LZbctTIAAAAd/tamm-cat.gif)
