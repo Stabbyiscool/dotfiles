bee8cc46600b5984ba4a4cbaa451dcff7a1bde0e
stab <stab@stab.ing>
2025-11-27 16:18:33 -0600

    hi

--- .config/sway/record.sh
+++ .config/sway/record.sh
@@ -11,9 +11,11 @@ OGFP="/home/$USER/records/$(date +'%Y-%m-%d_%H-%M-%S').mp4"
 if pgrep -x "wf-recorder" > /dev/null; then
     killall wf-recorder
 else
-    wf-recorder -g "$(slurp $full)" -f "$OGFP"&
+    AUDIO_DEV=$(pactl get-default-sink)
+    wf-recorder -g "$(slurp $full)" -f "$OGFP" --audio="$AUDIO_DEV.monitor" &
     REC_PID=$!
     wait $REC_PID
     ffmpeg -i "$OGFP" -vf "scale=-2:720" -vcodec libx264 -crf 32 -preset fast "${OGFP%.mp4}_comp720p.mp4"
     rm "$OGFP"
 fi
+
